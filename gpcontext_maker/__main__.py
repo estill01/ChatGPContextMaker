a3659a17ccb96b3bbce1462b4a6f266c0d55988d
estill01 <ehs.stillman@gmail.com>
2023-03-16 13:49:54 -0700

    Add: default gpcontext_ignore file + cli args to customize ignore file

--- gpcontext_maker/__main__.py
+++ gpcontext_maker/__main__.py
@@ -4,47 +4,99 @@
 import os
 import sys
 import argparse
-from pathlib import Path
+import fnmatch
 from datetime import datetime
+from pathlib import Path
 
-def concat_files_recursive(directory, output_file):
-    with open(output_file, 'w') as outfile:
-        for root, _, files in os.walk(directory):
+def read_ignore_patterns(ignore_file):
+    with open(ignore_file, "r") as f:
+        patterns = [line.strip() for line in f.readlines()]
+    return patterns
+
+def save_ignore_patterns(ignore_file, patterns):
+    with open(ignore_file, "w") as f:
+        for pattern in patterns:
+            f.write(pattern + "\n")
+
+def concat_files_recursive(input_dir, output_file, ignore_patterns=None):
+    if ignore_patterns is None:
+        ignore_patterns = []
+
+    with open(output_file, "w") as outfile:
+        for root, dirs, files in os.walk(input_dir):
+            dirs[:] = [d for d in dirs if not any(fnmatch.fnmatch(d, pattern) for pattern in ignore_patterns)]
+
             for file in files:
-                if file != os.path.basename(output_file):
-                    filepath = os.path.join(root, file)
-                    relative_path = os.path.relpath(filepath, directory)
-                    
-                    delimiter = f"\n{'#' * 80}\n# File: {relative_path}\n{'#' * 80}\n\n"
-                    outfile.write(delimiter)
-                    
-                    with open(filepath, 'r') as infile:
-                        outfile.write(infile.read())
-                        outfile.write('\n\n')
+                if any(fnmatch.fnmatch(file, pattern) for pattern in ignore_patterns):
+                    continue
+
+                file_path = os.path.join(root, file)
+                relative_path = os.path.relpath(file_path, input_dir)
+
+                delimiter = f"\n{'#' * 80}\n# File: {relative_path}\n{'#' * 80}\n\n"
+                outfile.write(delimiter)
+
+                with open(file_path, "r") as infile:
+                    content = infile.read()
+                    outfile.write(content)
+                    outfile.write("\n\n")
+
 
 def main():
-    parser = argparse.ArgumentParser(description='Concatenate files in a directory and its subdirectories into a single file.')
-    parser.add_argument('source_directory', help='The directory containing files to concatenate')
-    parser.add_argument('-o', '--output', help='Output directory (default: current directory)', default=os.getcwd())
-    parser.add_argument('-f', '--file', help='Output file name (default: context)', default='context')
-
+    parser = argparse.ArgumentParser(description="Concatenate files from a directory into a single file.")
+    parser.add_argument("input_dir", help="The input directory to concatenate.")
+    parser.add_argument("-i", "--ignore-file", help="A custom ignore file with patterns to ignore.", default=None)
+    parser.add_argument("-l", "--list-ignore", action="store_true", help="List current ignore patterns.")
+    parser.add_argument("-a", "--add-ignore", nargs="+", help="Add patterns to ignore.")
+    parser.add_argument("-r", "--remove-ignore", nargs="+", help="Remove patterns from ignore list.")
+    parser.add_argument("-m", "--merge-default", action="store_true", help="Merge the default ignore file with the .gpcontext_ignore file.")
+    parser.add_argument("-f", "--force-custom", action="store_true", help="Force using only the custom ignore file.")
     args = parser.parse_args()
 
-    source_directory = args.source_directory
-    output_directory = args.output
-    output_file_name = args.file
+    input_dir = args.input_dir
 
-    output_file_path = os.path.join(output_directory, output_file_name)
+    output_filename = "context_{}.txt".format(datetime.now().strftime("%Y%m%d_%H%M%S"))
+    output_file = Path.cwd() / output_filename
 
-    if not os.path.exists(output_directory):
-        os.makedirs(output_directory)
+    default_ignore_file = os.path.join(os.path.dirname(__file__), "default_gpcontext_ignore.txt")
+    user_ignore_file = args.ignore_file or ".gpcontext_ignore"
 
-    if os.path.isfile(output_file_path):
-        now = datetime.now().strftime("%Y%m%d_%H%M%S")
-        output_file_name = f"{output_file_name}_{now}"
-        output_file_path = os.path.join(output_directory, output_file_name)
+    if os.path.isfile(default_ignore_file):
+        default_patterns = read_ignore_patterns(default_ignore_file)
+    else:
+        print(f"Default ignore file '{default_ignore_file}' not found. No patterns will be ignored.")
+        default_patterns = []
 
-    concat_files_recursive(source_directory, output_file_path)
+    user_patterns = []
+    if os.path.isfile(user_ignore_file):
+        user_patterns = read_ignore_patterns(user_ignore_file)
 
-if __name__ == '__main__':
+    if args.merge_default:
+        user_patterns = list(set(user_patterns + default_patterns))
+        save_ignore_patterns(user_ignore_file, user_patterns)
+
+    if args.force_custom:
+        ignore_patterns = user_patterns
+    else:
+        ignore_patterns = list(set(default_patterns + user_patterns))
+
+    if args.list_ignore:
+        print("Ignore patterns:")
+        for pattern in ignore_patterns:
+            print(f"  - {pattern}")
+        return
+
+    if args.add_ignore:
+        user_patterns.extend(args.add_ignore)
+        save_ignore_patterns(user_ignore_file, user_patterns)
+
+    if args.remove_ignore:
+        user_patterns = [pattern for pattern in user_patterns if pattern not in args.remove_ignore]
+        save_ignore_patterns(user_ignore_file, user_patterns)
+
+    concat_files_recursive(input_dir, output_file, ignore_patterns)
+
+if __name__ == "__main__":
     main()
+
+
--- tests/test_gpcontext_maker_pytest.py
+++ tests/test_gpcontext_maker_pytest.py
@@ -19,6 +19,8 @@ def create_test_files(base_dir):
 
     return test_files
 
+
+# tests concatenation, but accounts for different orders files might be accessed in
 def test_make_context():
     with tempfile.TemporaryDirectory() as temp_dir:
         test_files = create_test_files(temp_dir)
@@ -27,15 +29,22 @@ def test_make_context():
 
         assert Path(output_file).is_file(), "Output file not created"
 
-        expected_output = ""
+        expected_output_sections = []
         for path, content in test_files.items():
             delimiter = f"\n{'#' * 80}\n# File: {path}\n{'#' * 80}\n\n"
-            expected_output += delimiter + content + "\n\n"
+            expected_output_sections.append(delimiter + content + "\n\n")
 
         with open(output_file, "r") as f:
             output_content = f.read()
 
-        assert output_content == expected_output, "Output content mismatch"
+        output_sections = output_content.split("\n" + "#" * 80)[1:]
+
+        # Sort sections for comparison
+        expected_output_sections.sort()
+        output_sections.sort()
+
+        assert output_sections == expected_output_sections, "Output content mismatch"
+
 
 if __name__ == "__main__":
     test_make_context()
